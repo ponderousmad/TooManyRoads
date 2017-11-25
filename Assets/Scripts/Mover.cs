6ebe5b6c33169a86995e3db9b1981a4c24c4f22a
Argilliath (Gabe) <argilliath@gmail.com>
2017-11-24 21:18:35 -0500

    Initial commit
    Mover and char script and test scene

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public Vector2 moveDirection;
+	public Vector2 moveSpeed;
+	public float moveTime;
+
+	private float flip;
+	private float currentTime;
+
+	// Use this for initialization
+	void Start () {
+		flip = 1.0f;
+		currentTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 moveDelta = moveSpeed * Time.deltaTime * flip;
+
+		Vector3 newPosition = transform.localPosition;
+		newPosition.x += moveDirection.x * moveDelta.x;
+		newPosition.y += moveDirection.y * moveDelta.y;
+		transform.localPosition = newPosition;
+
+		currentTime += Time.deltaTime;
+		if (currentTime >= moveTime) {
+			flip *= -1.0f;
+			currentTime = 0.0f;
+		}
+	}
+}
---
+++ Assets/Scripts/PhysCharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysCharacterController : MonoBehaviour {
+	public float movementSpeed;
+	public float jumpImpulse;
+	public LayerMask groundSurfaces;
+	public Transform basePoint;
+	public float groundCheckRadius;
+
+	private bool isGrounded = false;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+		if(Input.GetButtonDown("Jump"))
+		{
+			rigidBody.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		isGrounded = Physics2D.OverlapCircle (basePoint.position, groundCheckRadius);
+		Debug.Log ("IS GROUNDED: " + isGrounded);
+
+		Vector2 moveForce = rigidBody.velocity;
+		float forward = Input.GetAxis ("Horizontal") * movementSpeed;
+		moveForce.x = forward;
+
+		rigidBody.velocity = moveForce;
+	}
+}
