82a14e27911281d1ac7853df2a0bd70fd33f5a3c
Brendan Angles <brendanangles@McBook.local>
2017-11-25 11:52:58 -0500

    KinematicProjectile first stuff

--- Assets/Scripts/KinematicProjectile.cs
+++ Assets/Scripts/KinematicProjectile.cs
@@ -3,15 +3,32 @@ using System.Collections;
 
 public class KinematicProjectile : MonoBehaviour {
 
-    private Vector3 mVelocity = new Vector3();
+	private Vector2 mVelocity = new Vector2();
 
 	// Use this for initialization
 	void Start () {
-	
+		SetVelocity(new Vector2 (1.0f, 0.0f));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position = transform.position + mVelocity * Time.deltaTime;
+	void Update() {
+		Vector2 pos2D = new Vector2 (transform.position.x, transform.position.y);
+		RaycastHit2D result = Physics2D.Raycast(pos2D, mVelocity, mVelocity.magnitude * Time.deltaTime);
+		Collider2D hitObject = result.collider;
+		if (hitObject == null) {
+			transform.position = transform.position + new Vector3(mVelocity.x, mVelocity.y, 0) * Time.deltaTime;
+		} else {
+			Debug.Log (hitObject.GetType().FullName);
+			CollisionResponse cr = this.gameObject.GetComponent<CollisionResponse>();
+			if (cr != null) {
+				cr.Collision (hitObject);
+			}
+			Destroy (this.gameObject);
+		}
+	}
+
+	void SetVelocity(Vector2 vel)
+	{
+		mVelocity = vel;
 	}
 }
