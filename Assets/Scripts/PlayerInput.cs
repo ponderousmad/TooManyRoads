f93bf8db4c771905b96e79fb1ac66260202d49c4
Adrian Smith <agnomen@gmail.com>
2017-11-25 17:43:21 -0500

    Mostly working input on OSX

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -18,7 +18,7 @@ public class PlayerInput {
 
 	enum Axis
 	{
-		MoveX = 0,
+		MoveX = 0,  
 		MoveY = 1,
 		AimX = 2,
 		AimY = 3,
@@ -29,13 +29,14 @@ public class PlayerInput {
 	enum Button
 	{
 		Embiggen = 0,
-		Debigulate = 1
+		Debigulate = 1,
+        Press = 2
 	}
 
-	private static string[] WindowsAxisNames = { "x axis", "x axis", "4", "5", "9", "10" };
-	private static int[] WindowsButtonNumbers = { 0, 1 };
-	private static string[] OSXAxisNames = { "x axis", "y axis", "3", "4", "5", "6" };
-	private static int[] OSXButtonNumbers = { 0, 1 };
+	private static int[] WindowsAxisNames = { 0, 1, 3, 4, 8, 9 };
+	private static int[] WindowsButtonNumbers = { 7, 6, 0 };
+	private static int[] OSXAxisNames = { 0, 1, 2, 3, 4, 5 };
+	private static int[] OSXButtonNumbers = { 7, 6, 16 };
 
 	private static string PlatformName(RuntimePlatform platform)
 	{
@@ -51,7 +52,7 @@ public class PlayerInput {
 		return "";
 	}
 
-	private static string[] PlatformAxis(RuntimePlatform platform)
+	private static int[] PlatformAxis(RuntimePlatform platform)
 	{
 		switch(platform)
 		{
@@ -84,10 +85,32 @@ public class PlayerInput {
 		return "_P" + player.ToString() + PlatformName(platform);
 	}
 
-	static string AxisInput(Axis axis, int player, RuntimePlatform platform, string negativeKey, string positiveKey, string altNegative = "", string altPostive = "")
+	static string AnalogAxisInput(Axis axis, int player, RuntimePlatform platform, bool invert)
 	{
 		string axisName = axis.ToString() + Postfix(player, platform);
-		string axisValue = PlatformAxis(platform)[(int)axis];
+		int axisNumber = PlatformAxis(platform)[(int)axis];
+		return string.Format(@"
+  - serializedVersion: 3
+    m_Name: {0}
+    descriptiveName: 
+    descriptiveNegativeName: 
+    negativeButton:
+    positiveButton:
+    altNegativeButton:
+    altPositiveButton:
+    gravity: 0
+    dead: 0.02
+    sensitivity: 1
+    snap: 0
+    invert: {1}
+    type: 2
+    axis: {2}
+    joyNum: {3}", axisName, invert ? "1" : "0", axisNumber, player);
+	}
+
+    static string KeyboardAxisInput(Axis axis, int player, RuntimePlatform platform, string negativeKey, string positiveKey, string altNegative = "", string altPostive = "" )
+    {
+		string axisName = axis.ToString() + Postfix(player, platform);  
 		return string.Format(@"
   - serializedVersion: 3
     m_Name: {0}
@@ -97,14 +120,37 @@ public class PlayerInput {
     positiveButton: {2}
     altNegativeButton: {3}
     altPositiveButton: {4}
+    gravity: 3
+    dead: 0.001
+    sensitivity: 3
+    snap: 1
+    invert: 0
+    type: 0
+    axis: 0
+    joyNum: {5}", axisName, negativeKey, positiveKey, altNegative, altPostive, player);
+    }
+
+	static string ButtonAxisInput(Button button, int player, RuntimePlatform platform)
+	{
+		string buttonName = button.ToString() + Postfix(player, platform);
+		int buttonNumber = PlatformButton(platform)[(int)button];
+		return string.Format(@"
+  - serializedVersion: 3
+    m_Name: {0}
+    descriptiveName: 
+    descriptiveNegativeName: 
+    negativeButton:
+    positiveButton: joystick button {1}
+    altNegativeButton:
+    altPositiveButton:
     gravity: 1000
     dead: 0.001
     sensitivity: 1000
-    snap: 0
+    snap: 1
     invert: 0
-    type: 2
-    axis: {5}
-    joyNum: {6}", axisName, negativeKey, positiveKey, altNegative, altPostive, axisValue, player);
+    type: 0
+    axis: 0
+    joyNum: {2}", buttonName, buttonNumber, player);
 	}
 
 	static string ButtonInput(Button button, int player, RuntimePlatform platform, string key)
@@ -132,13 +178,24 @@ public class PlayerInput {
 
 	static IEnumerable<string> ControllerDefinition(int player, RuntimePlatform platform)
 	{
-		bool useKeys = player == 1;
-		yield return AxisInput(Axis.MoveX, player, platform, useKeys ? "a" : "", useKeys ? "d" : "");
-		yield return AxisInput(Axis.MoveY, player, platform, useKeys ? "w" : "", useKeys ? "s" : "", useKeys ? "space" : "");
-		yield return AxisInput(Axis.AimX, player, platform, useKeys ? "left" : "", useKeys ? "right" : "");
-		yield return AxisInput(Axis.AimY, player, platform, useKeys ? "down" : "", useKeys ? "up" : "");
-		yield return AxisInput(Axis.Embiggen, player, platform, useKeys ? "e" : "", "");
-		yield return AxisInput(Axis.Debigulate, player, platform, useKeys ? "q" : "", "");
+		if(player==1)
+        {
+            yield return KeyboardAxisInput(Axis.MoveX, player, platform, "a", "d");
+            yield return KeyboardAxisInput(Axis.MoveY, player, platform, "w", "s", "", "space");
+            yield return KeyboardAxisInput(Axis.AimX, player, platform, "left", "right");
+            yield return KeyboardAxisInput(Axis.AimY, player, platform, "down", "up");
+            yield return KeyboardAxisInput(Axis.Embiggen, player, platform, "", "e");
+            yield return KeyboardAxisInput(Axis.Debigulate, player, platform, "", "q");
+        }
+		yield return AnalogAxisInput(Axis.MoveX, player, platform, false);
+		yield return AnalogAxisInput(Axis.MoveY, player, platform, true);
+		yield return AnalogAxisInput(Axis.AimX, player, platform, false);
+		yield return AnalogAxisInput(Axis.AimY, player, platform, true);
+		yield return AnalogAxisInput(Axis.Embiggen, player, platform, false);
+		yield return AnalogAxisInput(Axis.Debigulate, player, platform, false);
+        yield return ButtonAxisInput(Button.Embiggen, player, platform);
+        yield return ButtonAxisInput(Button.Debigulate, player, platform);
+        yield return ButtonInput(Button.Press, player, platform, "space");
 	}
 
 	public static string AllDefinitions()
@@ -169,7 +226,11 @@ public class PlayerInput {
 		fireDebigulate = Button.Debigulate.ToString() + postfix;
 	}
 
-	private float GetAxis(string axis) { return Input.GetAxisRaw(axis); }
+	private float GetAxis(string axis) {
+        float value = Input.GetAxisRaw(axis);
+        Debug.Log("Getting " + axis + " = " + value.ToString());
+        return value;
+    }
 	private bool HasAim(float value) { return Mathf.Abs(value) > aimAxisDeadzone; }
 
 	public float AimX { get { return GetAxis(aimAxisX); } }
@@ -197,4 +258,12 @@ public class PlayerInput {
 
 	public bool FireEmbiggen { get { return GetAxis(fireEmbiggen) > fireThreshold; } }
 	public bool FireDebigulate { get { return GetAxis(fireDebigulate) > fireThreshold; } }
+
+    public bool MenuPress { get { return false; } }
+
+    public bool MenuUp { get { return MoveY < 0; } }
+    public bool MenuDown { get { return MoveY > 0; } }
+
+    public bool MenuLeft { get { return MoveX < 0; } }
+    public bool MenuRight { get { return MoveX > 0; } }
 }
