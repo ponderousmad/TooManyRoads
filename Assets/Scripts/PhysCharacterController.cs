57ae60fa426e2ce8faf58feb987b25c0dcaf0034
Argilliath (Gabe) <argilliath@gmail.com>
2017-11-25 12:12:31 -0500

    Movement update
    Now using axis raw, wall jump doesn't do anything right now because the x input overrides velocity, waiting for the smooth thingy

--- Assets/Scripts/PhysCharacterController.cs
+++ Assets/Scripts/PhysCharacterController.cs
@@ -8,10 +8,16 @@ public class PhysCharacterController : MonoBehaviour {
 	public float slope = 45.0f; // in degrees
 	public float wallSlope = 15.0f;
 	public int maxJumps = 2;
+	public float slideConstantGravity = -2.0f;
+	public Vector2 wallJumpImpulse;
 
 	// Debug variables
 	public Vector2 debugVelocity;
 	public bool debugIsClinging;
+	public Vector2 debugContactNormal;
+	public float debugLastDir;
+	public float debugRightDot;
+	public float debugRightDotCheck;
 
 	private bool isClinging;
 	private int jumpCount;
@@ -32,7 +38,12 @@ public class PhysCharacterController : MonoBehaviour {
 			Vector2 updatedVelocity = rigidBody.velocity;
 			updatedVelocity.y = 0;
 			rigidBody.velocity = updatedVelocity;
-			rigidBody.AddForce(new Vector2(0.0f, jumpImpulse), ForceMode2D.Impulse);
+
+			if (isClinging) {
+				rigidBody.AddForce (new Vector2 (wallJumpImpulse.x * -lastDir, wallJumpImpulse.y), ForceMode2D.Impulse);
+			} else {
+				rigidBody.AddForce (new Vector2 (0.0f, jumpImpulse), ForceMode2D.Impulse);
+			}
 			++jumpCount;
 		}
 	}
@@ -40,14 +51,25 @@ public class PhysCharacterController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector2 moveForce = rigidBody.velocity;
-		float forward = Input.GetAxis ("Horizontal") * movementSpeed;
+		float forward = Input.GetAxisRaw ("Horizontal") * movementSpeed;
+
 		moveForce.x = forward;
+		float newDir = 0.0f;
+		newDir = Mathf.Approximately(forward, 0.0f) ? 0.0f : Mathf.Sign (forward);
 
 		if (isClinging) { // Are we still clinging?
-			
+			if (lastDir != newDir) {
+				isClinging = false;
+			}
 		}
 
-		lastDir = Mathf.Sign (forward);
+		if (isClinging) {
+			moveForce.y = slideConstantGravity;
+		}
+		debugIsClinging = isClinging;
+
+		lastDir = newDir;
+		debugLastDir = lastDir;
 
 		debugVelocity = moveForce;
 		rigidBody.velocity = moveForce;
@@ -66,16 +88,28 @@ public class PhysCharacterController : MonoBehaviour {
 					transform.parent = collision.gameObject.transform;
 				}
 			} else {
-				if (Vector2.Dot(contacts[0].normal, Vector2.right * lastDir) > Mathf.Cos(Mathf.Deg2Rad * wallSlope)) {
-					isClinging = true;
+				debugRightDot = Vector2.Dot(contacts[0].normal, Vector2.right * -lastDir);
+				float rightDotCheck = Mathf.Cos (Mathf.Deg2Rad * wallSlope);
+				debugRightDotCheck = rightDotCheck;
+				if (Vector2.Dot(contacts[0].normal, Vector2.right * -lastDir) > rightDotCheck) {
+					OnClinged ();
 				}
 			}
+
+			debugContactNormal = contacts [0].normal;
 		}
 	}
 
 	void OnCollisionExit2D(Collision2D collision)
 	{
 		transform.parent = null;
+		isClinging = false;
+	}
+
+	void OnClinged()
+	{
+		isClinging = true;
+		jumpCount = 0;
 	}
 
 	void OnLanded()
