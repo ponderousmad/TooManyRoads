1515c334909a559eebd13cb30e9decc16ea54357
Brendan Angles <brendanangles@McBook.local>
2017-11-25 13:42:57 -0500

    -add fire rate and projectile velocity to ShootProjectile component

--- Assets/Scripts/ProjectileShooter.cs
+++ Assets/Scripts/ProjectileShooter.cs
@@ -7,19 +7,28 @@ public class ProjectileShooter : MonoBehaviour {
 	public GameObject projectileType1;
 	public GameObject projectileType2;
 
+	public float projectileSpeed = 10.0f;
+	public float fireRate = 1.0f;
+
+	private float mShootTimer;
+
 	// Use this for initialization
 	void Start () {
-		
+		mShootTimer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (mShootTimer > 0) {
+			mShootTimer -= Time.deltaTime;
+		}
+
 		float fire1 = Input.GetAxis ("Fire1");
-		if (fire1 > 0) {
+		if (fire1 > 0 && CanShoot()) {
 			ShootProjectile (projectileType1);
 		}
 		float fire2 = Input.GetAxis ("Fire2");
-		if (fire2 > 0) {
+		if (fire2 > 0 && CanShoot()) {
 			ShootProjectile (projectileType2);
 		}
 
@@ -32,12 +41,23 @@ public class ProjectileShooter : MonoBehaviour {
 			return;
 		}
 
-		Vector3 offset = new Vector3 (2.0f, 0, 0); // TODO: get from "Player Aim"
+		Vector3 offset = new Vector3 (2.0f, 0, 0); // TODO: get from "Player Aim. Also consider player scale."
 
 		GameObject projectile = Instantiate (projectileType, transform.position + offset, Quaternion.identity);
 		KinematicProjectile kinBehavior = projectile.GetComponent<KinematicProjectile> ();
 		if (kinBehavior != null) {
-			kinBehavior.SetVelocity (new Vector2 (1.0f, 0.0f));
+			kinBehavior.SetVelocity (new Vector2 (1.0f, 0.0f) * projectileSpeed);
 		}
+
+		mShootTimer = (1 / fireRate);
+	}
+
+	private bool CanShoot()
+	{
+		if (mShootTimer > 0) {
+			return(false);
+		}
+
+		return(true);
 	}
 }
