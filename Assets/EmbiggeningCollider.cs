e09665f7a977b42e563e852d4c35c1e9a21be1df
Brendan Angles <brendanangles@McBook.local>
2017-11-25 11:54:38 -0500

    Embiggening collision component, extending a "CollisionResponse"

---
+++ Assets/EmbiggeningCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmbiggeningCollider : CollisionResponse {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public override void Collision(Collider2D other)
+	//void OnCollision(Collider2D other)
+	//void OnCollision(Collider2D other)
+	//void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log ("collision!");
+		//Collider2D other = coll.collider;
+		//Collider2D other = (Collider2D)coll;
+		//Collider2D other = new Collider2D();
+		Embiggener embiggener = other.GetComponent<Embiggener>();
+		if(embiggener != null) 
+		{
+			embiggener.Embiggen(0.1f);
+		}
+	}
+}
