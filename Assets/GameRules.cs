61277444f63c370e922dbfeb01cccf0097c69a51
JakamoJones <jacqueswaller@hotmail.com>
2017-11-26 12:42:40 -0500

    * Moved a few scripts into Scripts folder  * Basic win/lose conditions. Pause camera and players on gameover

---
+++ Assets/GameRules.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRules : MonoBehaviour {
+
+    public delegate void GameOver();
+    public static event GameOver OnGameOver;
+
+    int playerCount = 0;
+    int deadPlayers = 0;
+
+    bool gameIsOver = false;
+    int timeToNextLevel = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPlayerSpawned(PlayerSpawner player)
+    {
+        ++playerCount;
+    }
+
+    public void OnPlayerEliminated(PlayerSpawner player)
+    {
+        ++deadPlayers;
+
+        if(playerCount == 1)
+        {
+            print("GAME OVER! PLEASE DEPOSIT 40 QUARTERS!");
+            gameIsOver = true;
+            OnGameOver();
+        } else if(deadPlayers == playerCount - 1)
+        {
+            print("We have a winner!");
+            gameIsOver = true;
+            OnGameOver();
+        }
+    }
+}
