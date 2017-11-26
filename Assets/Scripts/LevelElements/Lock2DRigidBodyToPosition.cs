47cdc7d4f55db432ce1882f01bb7b442e6e714f1
Mark Cautillo <mark.cautillo@gmail.com>
2017-11-25 20:53:08 -0500

    * There's a wrecking ball now to play with :)

---
+++ Assets/Scripts/LevelElements/Lock2DRigidBodyToPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock2DRigidBodyToPosition : MonoBehaviour {
+
+    public GameObject target;
+    public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        body.MovePosition(target.transform.position);
+       // body.MoveRotation(target.transform.rotation.z);
+
+    }
+}
